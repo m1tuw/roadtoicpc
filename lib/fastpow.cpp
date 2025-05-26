c156412f2b961c39775c9a40f9b4ca243b3b1e74
mitu <111256694+m1tuw@users.noreply.github.com>
2025-05-26 00:44:02 -0400

    changed MOD to mod

--- lib/fastpow.cpp
+++ lib/fastpow.cpp
@@ -1,15 +1,15 @@
 ll fastpow(ll a, ll b){
-  a %= MOD;
+  a %= mod;
   ll ans=1;
   while(b > 0){
     if(b & 1)
-      ans = ans * a % MOD;
-    a = a * a % MOD;
+      ans = ans * a % mod;
+    a = a * a % mod;
     b >>= 1;
   }
   return ans;
 }
 
 ll modinv(ll x){
-  return fastpow(x, MOD-2);
+  return fastpow(x, mod-2);
 }
